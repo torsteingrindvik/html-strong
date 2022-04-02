7151660afdae5eadaade7ca61d5e2b66221def71
Torstein Grindvik <torstein.grindvik@nordicsemi.no>
2022-04-02 23:27:41 +0200

    HN getting closer
    Signed-off-by: Torstein Grindvik <torstein.grindvik@nordicsemi.no>

--- examples/hacker-news.rs
+++ examples/hacker-news.rs
@@ -120,49 +120,105 @@ mod hacker_news {
     }
 
     pub mod post {
-        use std::time::{Duration, Instant};
-
-        use tracing::info;
-
+        use super::*;
         use crate::SharedState;
 
-        use super::*;
+        use anyhow::Result;
+        use futures::{future, stream::FuturesOrdered, FutureExt, StreamExt};
+        use serde::Deserialize;
+        use std::time::{Duration, Instant};
+        use tracing::{info, trace, warn};
 
-        #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+        #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
         pub struct Post {
-            pub upvotes: usize,
-            pub author: String,
+            /// Author
+            pub by: String,
+            /// Number of comments
+            pub descendants: usize,
+            /// Story id
             pub id: usize,
-            pub comments: usize,
-            pub rank: usize,
+            /// Comments
+            // pub kids: usize,
+            /// Upvotes
+            pub score: usize,
+            /// Time in ?? format
+            pub time: usize,
+            /// Story title
             pub title: String,
-            pub url: String,
-            pub shorturl: String,
+            /// Story url. TODO: Short version?
+            pub url: Option<String>,
+
+            /// Not in JSON, will be set by us
+            pub rank: Option<usize>,
+        }
+
+        async fn get_new_story_ids() -> Result<Vec<usize>> {
+            Ok(
+                reqwest::get("https://hacker-news.firebaseio.com/v0/topstories.json")
+                    .await?
+                    .json()
+                    .await?,
+            )
+        }
+
+        async fn get_story(id: usize) -> Result<post::Post> {
+            let story_raw = reqwest::get(format!(
+                "https://hacker-news.firebaseio.com/v0/item/{id}.json"
+            ))
+            .await?;
+            trace!("Story raw: {story_raw:#?}");
+
+            Ok(story_raw.json().await?)
+        }
+
+        async fn get_posts() -> Result<Vec<post::Post>> {
+            let mut story_ids = get_new_story_ids().await?;
+            info!("Pulled {} story ids", story_ids.len());
+            story_ids.truncate(50);
+
+            let mut futures = FuturesOrdered::new();
+
+            for id in story_ids {
+                futures.push(get_story(id).map(move |fut| (fut, id)));
+            }
+
+            let now = Instant::now();
+
+            let posts = futures
+                .inspect(|(fut, id)| {
+                    if let Err(e) = fut {
+                        warn!("Problem (id: {id}) getting post: {e:?}");
+                    }
+                })
+                .filter_map(|(fut, _)| future::ready(fut.ok()))
+                .collect::<Vec<_>>()
+                .await;
+
+            info!("Posts resolved in {:?}", now.elapsed());
+
+            Ok(posts)
         }
 
         pub async fn worker(state: SharedState) {
             info!("API worker started");
-            let mut n = 0;
 
             loop {
-                let post = post::Post {
-                    upvotes: n * 10,
-                    author: "authoria".into(),
-                    id: n,
-                    comments: 100 + n,
-                    rank: n + 1,
-                    title: format!("This is post with index {n}"),
-                    url: format!("https://nrk.no/{}", n),
-                    shorturl: "nrk.no".into(),
+                let posts = match get_posts().await {
+                    Ok(posts) => posts,
+                    Err(e) => {
+                        warn!("Couldn't fetch stories: {e:?}");
+                        continue;
+                    }
                 };
 
-                let now = Instant::now();
-                state.0.write().await.push(post);
+                {
+                    let now = Instant::now();
+                    let mut state = state.0.write().await;
+                    *state = posts;
+                    info!("New post added (held write lock for {:?})", now.elapsed());
+                }
 
-                info!("New post added (held write lock for {:?})", now.elapsed());
-
-                tokio::time::sleep(Duration::from_secs(5)).await;
-                n += 1;
+                tokio::time::sleep(Duration::from_secs(60)).await;
             }
         }
 
@@ -173,6 +229,35 @@ mod hacker_news {
 
         #[cached]
         fn post(post: Post) -> Node {
+            let link_thing: Node = if let Some(url) = post.url {
+                o(td())
+                    .add_class("title")
+                    .kid(
+                        o(A::href(&url))
+                            .add_class("titlelink")
+                            .add_text(&post.title),
+                    )
+                    .kid(
+                        o(Span)
+                            .add_class("sitebit comhead")
+                            .add_text(" (")
+                            .kid(
+                                o(A::href(&format!("from?site={}", &url)))
+                                    .kid(o(Span).add_class("sitestr"))
+                                    .add_text(&url),
+                            )
+                            .add_text(")"),
+                    )
+            } else {
+                Node::root()
+            };
+
+            let comment_text = if post.descendants == 0 {
+                "discuss".into()
+            } else {
+                format!("{} comments", post.descendants)
+            };
+
             Node::root()
                 .kid(
                     o(Tr)
@@ -182,7 +267,7 @@ mod hacker_news {
                             o(td()).add_class("title-rank").kid(
                                 o(Span)
                                     .add_class("rank")
-                                    .add_text(&format!("{}.", post.rank)),
+                                    .add_text(&format!("{}.", post.rank.expect("Must set rank"))),
                             ),
                         )
                         .kid(
@@ -192,37 +277,18 @@ mod hacker_news {
                                     .kid(o(Div).add_class("votearrow").set_title("upvote")),
                             ),
                         )
-                        .kid(
-                            o(td())
-                                .add_class("title")
-                                .kid(
-                                    o(A::href(&post.url))
-                                        .add_class("titlelink")
-                                        .add_text(&post.title),
-                                )
-                                .kid(
-                                    o(Span)
-                                        .add_class("sitebit comhead")
-                                        .add_text(" (")
-                                        .kid(
-                                            o(A::href(&format!("from?site={}", &post.shorturl)))
-                                                .kid(o(Span).add_class("sitestr"))
-                                                .add_text(&post.shorturl),
-                                        )
-                                        .add_text(")"),
-                                ),
-                        ),
+                        .kid(link_thing),
                 )
                 .kid(
                     o(Tr).kid(Td::colspan(2)).kid(
                         o(td())
                             .add_class("subtext")
-                            .kid(o(Span).add_text(&format!("{} points", post.upvotes)))
+                            .kid(o(Span).add_text(&format!("{} points", post.score)))
                             .add_text(" by ")
                             .kid(
-                                o(A::href(&format!("user?id={}", post.author)))
+                                o(A::href(&format!("user?id={}", post.by)))
                                     .add_class("hnuser")
-                                    .add_text(&post.author)
+                                    .add_text(&post.by)
                                     .add_text(ONE_SPACE),
                             )
                             .kid(
@@ -243,7 +309,7 @@ mod hacker_news {
                             .add_text(PIPE_DELIMITER)
                             .kid(
                                 o(A::href(&format!("item?id={}", { post.id })))
-                                    .add_text(&format!("{} comments", post.comments)),
+                                    .add_text(&comment_text),
                             ),
                     ),
                 )
@@ -260,22 +326,10 @@ mod hacker_news {
     fn body_posts(posts: Vec<post::Post>) -> Node {
         let mut items = o(Table).add_class("itemlist");
 
-        for post in posts {
+        for (rank, mut post) in posts.into_iter().enumerate() {
+            post.rank = Some(rank + 1);
             items.push_kid(post)
         }
-
-        // for n in 0..25 {
-        //     items.push_kid(post::Post {
-        //         upvotes: n * 10,
-        //         author: "authoria".into(),
-        //         id: n,
-        //         comments: 100 + n,
-        //         rank: n + 1,
-        //         title: format!("This is post with index {n}"),
-        //         url: format!("https://nrk.no/{}", n),
-        //         shorturl: "nrk.no".into(),
-        //     })
-        // }
 
         o(Tr).kid(o(td()).kid(items))
     }
